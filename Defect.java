f791ae6e9ac91b791ee3b3e69331f132cb4c29a7
JohnCrisanto <jcrisanto87@gmail.com>
2016-09-04 20:27:47 -0700

    Revert "John's branch"

---
+++ Defect.java
@@ -0,0 +1,103 @@
+public class Defect {
+	public String application;	
+	public String assignee;
+	public Integer defectName;	
+	public String description;
+	public String priority;
+	public String status;	
+	public String summary;
+	
+
+	//Constructor	
+
+	public Defect(String application, String assignee, Integer defectName,
+			String description, String priority, String status, String summary) {
+		this.application = application;
+		this.assignee = assignee;
+		this.defectName = defectName;
+		this.description = description;
+		this.priority = priority;
+		this.status = status;
+		this.summary = summary;
+	}
+
+	public Defect() {}
+	
+	//Getters and setters
+
+	public String getapplication() {
+		return application;
+	}
+
+	public void setapplication(String application) {
+		this.application = application;
+	}
+
+	public Integer getDefectName() {
+		return defectName;
+	}
+
+	public void setDefectName(Integer defectName) {
+		this.defectName = defectName;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getAssignee() {
+		return assignee;
+	}
+
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+	
+	public void Copy(Defect defect) {
+		application = defect.application;
+		defectName = defect.defectName;
+		summary = defect.summary;
+		status = defect.status;
+		priority = defect.priority;
+		assignee = defect.assignee;
+		description = defect.description;
+	}
+
+	@Override
+	public String toString() {
+		return "Defect [application=" + application + ", defectName=" + defectName
+				+ ", summary=" + summary + ", status=" + status + ", priority="
+				+ priority + ", assignee=" + assignee + ", description="
+				+ description + "]";
+	}
+}
---
+++ README-ListofDefects.txt
@@ -0,0 +1,18 @@
+Team
+
+In order to make a dynamic web page to list only the open defects , I needed
+to pass an Array List to the jsp.  For this to happen, I needed to import the Defect class
+into the jsp.  This required that it be in a package.  I named the package teamc.  You will
+need to create a package within Eclipse and place the servlets and java files in it.  The web.xml is setup for this.
+
+My files that I downloaded are
+OpenDefectPage.jsp
+OpenDefects.java
+DetailTester.java (used to verify that clicking on link in form sent the parameter onward).
+web.xml
+
+In order to run this you need to call on the servlet like  we did on the first servlet homework.
+I will create an index.html that passes everything to the servlet next.
+
+Also, all of the formatting was lost on the OpenDefects.jsp.  I will try and fiqure this out.
+
